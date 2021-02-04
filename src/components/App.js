21704b874dbc041c713f514e1eb171f968dee530
Jacob Marthaller <https://github.com/jmarthaller>
2021-02-04 17:53:34 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+
+function App() {
+
+  const [dogPic, setDogPic] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
+
+
+     useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setDogPic(data.message);
+            setIsLoaded(true);
+        });
+      }, []);
+
+
+      if (!isLoaded) {
+          return <h3>Loading...</h3>;
+      } else {
+        return  <img src={dogPic} alt="A Random Dog"/>
+      }
+        //   {isLoaded ? null : <p>Loading...</p> }
+    
+      
+    
+   
+}
+
+
+
+export default App;
